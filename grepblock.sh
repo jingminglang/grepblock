94925600ece12d8f9208d82651a6a03e97ace67b
jingminglang <cdjingminglang@jd.com>
2017-07-26 15:05:31 +0800

    modify

--- grepblock.sh
+++ grepblock.sh
@@ -7,6 +7,7 @@ IFS=''
 # () {} [] ...
 block=""
 str=""
+# filename
 file=""
 # string or filename
 add=""
@@ -16,7 +17,7 @@ function is_file() {
 }
 
 function encode() {
-   cat $1 | sed 's/\\/\\\\/g' | sed ":a;N;s/\n/\\\n/g;ta"   | sed -e "s/'/\\\x27/g"  -e "s#/#\\\/#g"
+    cat $1 | sed 's/\\/\\\\/g' | sed ":a;N;s/\n/\\\n/g;ta"   | sed -e "s/'/\\\x27/g"  -e "s#/#\\\/#g"
 }
 
 while getopts :a:b:s:f: name
@@ -43,13 +44,28 @@ do
     esac
 done
 
+b=''
+case $block in
+    '()') 
+	b='('
+	;;
+    '{}') 
+	b='{'
+	;;
+    '[]') 
+	b='['
+	;;
+    '<>') 
+	b='<'
+	;;
+esac
 
 if [ -z "$add" ]
 then
-    s="sed  -n  '/"$str"/ {:c;N;H;s/[^"$block"]//g;:a;s/"$block"//g;ta;/^$/s/$/$/;tb;s/.*//g;x;bc;:b;g;s/^\n\+//g;p}' "$file
+    s="sed  -n  '/"$str"/ {:c;N;H;s/[^"$block"]//g;s/"$b"/{/g;ta;tc;:a;s/"$block"//g;ta;/^$/s/$/$/;tb;s/.*//g;x;bc;:b;g;s/^\n\+//g;p}' "$file
     eval $s
 else
-    s="sed  '/"$str"/ {:c;N;H;s/[^"$block"]//g;:a;s/"$block"//g;ta;/^$/s/$/$/;tb;s/.*//g;x;bc;:b;g;s/^\n\+//g;s/\(.*\)/\1\n"$add"\n/g}' "$file
+    s="sed  '/"$str"/ {:c;N;H;s/[^"$block"]//g;s/"$b"/{/g;ta;tc;:a;s/"$block"//g;ta;/^$/s/$/$/;tb;s/.*//g;x;bc;:b;g;s/^\n\+//g;s/\(.*\)/\1\n"$add"\n/g}' "$file
+
     eval $s
 fi
-
